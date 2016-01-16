85a614cbc39028e31ce4bb47ff645232f7892fc2
Student Gauss <student.gauss@gmail.com>
2016-01-16 06:42:36 -0800

    Initial Commit

---
+++ BackPropagationNN/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  BackPropagationNN
+//
+//  Created by Motonari Ito on 1/16/16.
+//  Copyright © 2016 Student Gauss. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
