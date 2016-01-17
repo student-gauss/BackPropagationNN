914fdcd25496206446c541d6db5fecc747b621fc
Student Gauss <student.gauss@gmail.com>
2016-01-17 09:45:41 -0800

    Working version.

--- BackPropagationNN/BackPropagationNN.hpp
+++ BackPropagationNN/BackPropagationNN.hpp
@@ -5,16 +5,27 @@
 class Matrix
 {
 public:
-    typedef std::vector<float> Vector;
-
 private:
-    std::vector<Vector> data;
+    std::vector<std::vector<float>> data;
 public:
     friend std::vector<float> operator*(std::vector<float> const& v, Matrix const& M);
+    friend Matrix operator*(Matrix const& lhs, Matrix const& rhs);
+    friend Matrix operator*(Matrix const& lhs, float v);
 
     Matrix(int numberOfRows, int numberOfColumns)
-        :data(numberOfRows, Vector(numberOfColumns))
+        :data(numberOfRows, std::vector<float>(numberOfColumns))
     {
+    }
+
+    Matrix(int numberOfRows, int numberOfColumns, std::vector<float> v)
+        :data(numberOfRows, std::vector<float>(numberOfColumns))
+    {
+        int index = 0;
+        for (int rowIndex = 0; rowIndex != numberOfRows; ++rowIndex) {
+            for (int columnIndex = 0; columnIndex != numberOfColumns; ++columnIndex) {
+                data[rowIndex][columnIndex] = v[index++];
+            }
+        }
     }
 
     void initializeWithRandom(std::mt19937& randomGenerator) {
@@ -25,11 +36,54 @@ public:
             }
         }
     }
+
+    void set(int rowIndex, int columnIndex, float v) {
+        data[rowIndex][columnIndex] = v;
+    }
+
+    float get(int rowIndex, int columnIndex) const {
+        return data[rowIndex][columnIndex];
+    }
+
+    int rowCount() const
+    {
+        return static_cast<int>(data.size());
+    }
+
+    int columnCount() const
+    {
+        return static_cast<int>(data[0].size());
+    }
+
+    std::vector<float> asVector() const {
+        // As optimization, if row count is 1, return the column
+        // vector as is.
+        if (rowCount() == 1) {
+            return data[0];
+        }
+
+        std::vector<float> result(rowCount() * columnCount());
+        int index = 0;
+        for (int rowIndex = 0; rowIndex < rowCount(); ++rowIndex) {
+            for (int columnIndex = 0; columnIndex < columnCount(); ++columnIndex) {
+                result[index++] = data[rowIndex][columnIndex];
+            }
+        }
+        return result;
+    }
+
+    void operator-=(Matrix const& operand) {
+        for (int rowIndex = 0; rowIndex != rowCount(); rowIndex++) {
+            for (int columnIndex = 0; columnIndex != columnCount(); columnIndex++) {
+                set(rowIndex, columnIndex, get(rowIndex, columnIndex) - operand.get(rowIndex, columnIndex));
+            }
+        }
+    }
 };
 
 inline std::vector<float> operator*(const std::vector<float>& v, const Matrix& M)
 {
-    auto data = M.data;
+    auto& data = M.data;
     std::vector<float> result(data[0].size(), 0.0f);
 
     for (int rowIndex = 0; rowIndex < data.size(); ++rowIndex) {
@@ -41,12 +95,48 @@ inline std::vector<float> operator*(const std::vector<float>& v, const Matrix& M
     return result;
 }
 
+inline Matrix operator*(Matrix const& lhs, Matrix const& rhs)
+{
+    if (lhs.columnCount() != rhs.rowCount()) {
+        throw std::logic_error("incompatible matrices");
+    }
+
+    const int resultRowCount = lhs.rowCount();
+    const int resultColumnCount = rhs.columnCount();
+
+    Matrix result(lhs.rowCount(), rhs.columnCount());
+
+    for (int resultRowIndex = 0; resultRowIndex != resultRowCount; ++resultRowIndex) {
+        for (int resultColumnIndex = 0; resultColumnIndex != resultColumnCount; ++resultColumnIndex) {
+            const int count = lhs.columnCount();
+            float v = 0;
+            for (int index = 0; index != count; ++index) {
+                v += lhs.get(resultRowIndex, index) * rhs.get(index, resultColumnIndex);
+            }
+            result.set(resultRowIndex, resultColumnIndex, v);
+        }
+    }
+    return result;
+}
+
+inline Matrix operator*(Matrix const& m, float multiplier)
+{
+    Matrix result = m;
+    for (int rowIndex = 0; rowIndex != result.rowCount(); rowIndex++) {
+        for (int columnIndex = 0; columnIndex != result.columnCount(); columnIndex++) {
+            result.set(rowIndex, columnIndex, result.get(rowIndex, columnIndex) * multiplier);
+        }
+    }
+    return result;
+}
+
 class Layer
 {
 private:
     int layerIndex;
-    std::vector<float> inputs;
-    std::vector<float> outputs;
+    std::vector<float> currentInputs;
+    std::vector<float> currentOutputs;
+    std::vector<float> sensitivity;
     float learningRate;
     Matrix weights;
 public:
@@ -56,7 +146,7 @@ public:
           float learningRate,
           std::mt19937& randomGenerator)
         :layerIndex(layerIndex),
-         inputs(numberOfInputs),
+         currentInputs(numberOfInputs),
          learningRate(learningRate),
          weights(numberOfInputs, numberOfOutputs)
     {
@@ -66,21 +156,46 @@ public:
 
     void setInputs(std::vector<float> inputs)
     {
-        this->inputs = inputs;
+        currentInputs = inputs;
 
-        std::vector<float> nonLinearizedInput(this->inputs.size());
+        std::vector<float> nonLinearizedInput(currentInputs.size());
         if (layerIndex != 0) {
-            std::transform(inputs.begin(), inputs.end(), nonLinearizedInput.begin(), tanh);
+            std::transform(currentInputs.begin(), currentInputs.end(), nonLinearizedInput.begin(), tanh);
         } else {
-            std::copy(inputs.begin(), inputs.end(), nonLinearizedInput.begin());
+            std::copy(currentInputs.begin(), currentInputs.end(), nonLinearizedInput.begin());
         }
 
-        this->outputs = nonLinearizedInput * weights;
+        currentOutputs = nonLinearizedInput * weights;
     }
 
     std::vector<float> getOutputs() const
     {
-        return this->outputs;
+        return currentOutputs;
+    }
+
+    std::vector<float> computeSensitivity(std::vector<float> nextLayerSensitivity)
+    {
+        Matrix nextLayerSensitivityMatrix(static_cast<int>(nextLayerSensitivity.size()), 1, nextLayerSensitivity);
+        sensitivity = (weights * nextLayerSensitivityMatrix).asVector();
+
+        std::transform(sensitivity.begin(), sensitivity.end(), sensitivity.begin(), [](float v) { return 1 - tanh(v) * tanh(v); });
+
+        return sensitivity;
+    }
+
+    void updateWeight(std::vector<float> nextLayerSensitivity)
+    {
+        std::vector<float> nonLinearizedInput(currentInputs.size());
+        if (layerIndex != 0) {
+            std::transform(currentInputs.begin(), currentInputs.end(), nonLinearizedInput.begin(), tanh);
+        } else {
+            std::copy(currentInputs.begin(), currentInputs.end(), nonLinearizedInput.begin());
+        }
+
+        const int inputSize = static_cast<int>(currentInputs.size());
+        const int outputSize = static_cast<int>(nextLayerSensitivity.size());
+
+        weights -= Matrix(inputSize, 1, nonLinearizedInput) * Matrix(1, outputSize, nextLayerSensitivity) * learningRate;
     }
 };
 
@@ -103,23 +218,50 @@ public:
             Layer layer(static_cast<int>(layerIndex),
                         componentSizeList[layerIndex],
                         componentSizeList[layerIndex + 1],
-                        0.01,
+                        0.01, // learning rate
                         randomGenerator);
             layers.push_back(layer);
         }
     }
 
-    void train(std::vector<float> inputs, std::vector<float> expectedOutputs)
+    float train(std::vector<float> inputs, std::vector<float> expectedOutputs)
+    {
+        auto actualOutputs = test(inputs);
+
+        std::vector<float> outputSensitivity(actualOutputs.size());
+        std::transform(actualOutputs.begin(), actualOutputs.end(),
+                       expectedOutputs.begin(),
+                       outputSensitivity.begin(),
+                       [](float actual, float expected){ return 2 * (actual - expected); });
+
+        int layerCount = static_cast<int>(layers.size());
+
+        std::vector<float> inputSensitivity;
+        for (int layerIndex = layerCount - 1; layerIndex >= 0; --layerIndex) {
+            Layer& layer = layers[layerIndex];
+
+            // calculate sensitivity by the sensitivity of the next
+            // box.
+            inputSensitivity = layer.computeSensitivity(outputSensitivity);
+            layer.updateWeight(outputSensitivity);
+
+            outputSensitivity = inputSensitivity;
+        }
+
+        float error = inner_product(actualOutputs.begin(), actualOutputs.end(),
+                                    expectedOutputs.begin(), 0.0,
+                                    std::plus<float>(),
+                                    [](float v1, float v2) { return (v1 - v2) * (v1 - v2); } );
+        return error;
+    }
+
+    std::vector<float> test(std::vector<float> inputs)
     {
         auto layerInputs = inputs;
-        for (auto layer : layers) {
+        for (auto& layer : layers) {
             layer.setInputs(layerInputs);
             layerInputs = layer.getOutputs();
         }
-
-        auto actualOutputs = layerInputs;
-
+        return layerInputs;
     }
-
-
 };
--- BackPropagationNN/main.cpp
+++ BackPropagationNN/main.cpp
@@ -2,11 +2,26 @@
 
 #include <iostream>
 
+using namespace std;
+
 int main(int argc, const char * argv[]) {
     BackPropagationNN nn({2, 3, 2});
 
-    nn.train({0.2, 0.3}, {0.1, 0.0});
+    float error;
+    for(int i = 0; i < 1000000; i++) {
+        error = nn.train({0.2, 0.3}, {1.0, 0.0});
+        error = nn.train({0.5, 0.1}, {0.0, 1.0});
+    }
 
+    cout << "Test!" << endl;
+    auto v1 = nn.test({0.2, 0.3});
+    auto v2 = nn.test({0.5, 0.1});
+
+    for (auto v : v1) cout << v << ' ';
+    cout << endl;
+
+    for (auto v : v2) cout << v << ' ';
+    cout << endl;
 
     return 0;
 }
