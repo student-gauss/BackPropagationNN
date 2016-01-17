a6a808b82cd001298a27e038b63d63b491bbea51
Student Gauss <student.gauss@gmail.com>
2016-01-17 10:30:51 -0800

    Add timer.

--- BackPropagationNN/main.cpp
+++ BackPropagationNN/main.cpp
@@ -7,11 +7,14 @@ using namespace std;
 int main(int argc, const char * argv[]) {
     BackPropagationNN nn({2, 3, 2});
 
+    auto start = std::clock();
     float error;
-    for(int i = 0; i < 1000000; i++) {
+    for(int i = 0; i < 100000; i++) {
         error = nn.train({0.2, 0.3}, {1.0, 0.0});
         error = nn.train({0.5, 0.1}, {0.0, 1.0});
     }
+
+    cout << "Elapsed " << (std::clock() - start) / (double)CLOCKS_PER_SEC << " seconds." << endl;
 
     cout << "Test!" << endl;
     auto v1 = nn.test({0.2, 0.3});
